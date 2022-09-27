4e8a2de34b32bca147f6a5575a93c452f8465ef4
BCarnaval <antoinedelagrave@hotmail.com>
2022-09-27 17:47:11 -0400

    SUDO

---
+++ qvim/init.lua
@@ -0,0 +1,17 @@
+local config = {
+	-- set vim options here (vim.<first_key>.<second_key> =  value)
+	options = {
+		opt = {
+			relativenumber = true, -- sets vim.opt.relativenumber
+			breakindent = true,
+			textwidth = 74,
+			wrap = true,
+			linebreak = true,
+			tabstop = 8,
+			shiftwidth = 4,
+			expandtab = true,
+		},
+	},
+}
+
+return config
--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+CURR=$( pwd )
+CONFIG=~/.config/qvim
 LINK_DIR=/usr/local/bin/qvim
 DESTINATION=/usr/local/share
 
 setup () {
+    # Handles command
     sudo chmod +x ./qvim.sh
-    cp ./qvim.sh ${DESTINATION}
+    sudo cp ${CURR}/qvim.sh ${DESTINATION}
 
+    # Symlinks .config
+    ln -s ${CURR}/qvim ${CONFIG}
+
+    # Symlinks command 'qvim'
     if [[ -L ${LINK_DIR} ]]; then
         sudo rm ${LINK_DIR}
         sudo ln -s ${DESTINATION}/${PROJ}/qvim.sh ${LINK_DIR}
